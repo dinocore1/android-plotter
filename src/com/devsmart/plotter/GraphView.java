b459723b3d37979221a8b2be7571b784dab57b12
Paul Soucy <paul@psoucywork.(none)>
2013-08-19 12:36:08 -0400

    more work

--- src/com/devsmart/plotter/GraphView.java
+++ src/com/devsmart/plotter/GraphView.java
@@ -3,21 +3,23 @@ package com.devsmart.plotter;
 import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Matrix.ScaleToFit;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.Matrix.ScaleToFit;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import com.devsmart.BackgroundTask;
@@ -32,8 +34,9 @@ public abstract class GraphView extends View {
 	private Bitmap mFrontBuffer;
 	private Matrix mTransformMatrix = new Matrix();
 	private Paint mDrawPaint = new Paint();
-	private Future<?> mDrawFuture;
-	private GestureDetector mGestureDetector;
+	private BackgroundDrawTask mBackgroundDrawTask;
+	private GestureDetector mPanGestureDetector;
+	private ScaleGestureDetector mScaleGestureDetector;
 
 	//draw prefs
 	protected boolean mDrawXAxis;
@@ -42,6 +45,8 @@ public abstract class GraphView extends View {
 	protected Paint mAxisLabelPaint = new Paint();
 	protected Rect mPlotMargins = new Rect();
 	protected int mBackgroundColor;
+	protected float mXAxisDevision;
+	protected float mYAxisDevision;
 
 	public GraphView(Context context) {
 		super(context);
@@ -54,20 +59,23 @@ public abstract class GraphView extends View {
 	}
 
 	private void init() {
-		mGestureDetector = new GestureDetector(mSimpleGestureListener);
+		mPanGestureDetector = new GestureDetector(mSimpleGestureListener);
+		mScaleGestureDetector = new ScaleGestureDetector(getContext(), mSimpleScaleGestureListener);
 		mDrawPaint.setFilterBitmap(true);
 		mViewPort.set(-1, -1, 1, 1);
 		mTransformMatrix.reset();
 
 		//defaults
 		mDrawXAxis = true;
+		mXAxisDevision = 1.0f;
 		mDrawYAxis = true;
+		mYAxisDevision = 1.0f;
 		mPlotMargins.set(10, 10, 10, 10);
 		mAxisColor = Color.DKGRAY;
 		mAxisLabelPaint.setColor(Color.DKGRAY);
 		mAxisLabelPaint.setTextSize(20.0f);
-		
 		mBackgroundColor = Color.WHITE;
+
 	}
 
 	public void addSeries(Series series) {
@@ -89,10 +97,10 @@ public abstract class GraphView extends View {
 				//new RectF(mPlotMargins.left, mPlotMargins.top, canvasWidth-mPlotMargins.right, canvasHeight-mPlotMargins.bottom),
 				new RectF(0,0,canvasWidth,canvasHeight),
 				ScaleToFit.FILL);
-		
+
 		matrix.postScale(1, -1);
 		matrix.postTranslate(0, canvasHeight);
-		
+
 		return matrix;
 	}
 
@@ -104,32 +112,37 @@ public abstract class GraphView extends View {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		
+
 		final int action = MotionEventCompat.getActionMasked(event);
 		switch(action){
 		case MotionEvent.ACTION_UP:
 			updateViewport();
 			break;
 		}
-		
-		boolean retval = mGestureDetector.onTouchEvent(event);
+
+		boolean retval = mPanGestureDetector.onTouchEvent(event);
+		retval |= mScaleGestureDetector.onTouchEvent(event);
 		return retval;
 	}
 
-	protected void updateViewport(){	
-		
+	protected void updateViewport(){
+		RectF newViewport = getDisplayViewPort();
+		drawFrame(newViewport);
+	}
+
+	public RectF getDisplayViewPort(){
 		Matrix m = new Matrix();
 		mTransformMatrix.invert(m);
-		
-		RectF screen = new RectF(0,0, mFrontBuffer.getWidth(), mFrontBuffer.getHeight());
+
+		RectF screen = new RectF(0,0, getMeasuredWidth(), getMeasuredHeight());
 		m.mapRect(screen);
-		
+
 		Matrix viewPortTransform = getViewportToScreenMatrix(getMeasuredWidth(), getMeasuredHeight(), mViewPort);
 		Matrix screenToViewPort = new Matrix();
 		viewPortTransform.invert(screenToViewPort);
-		
+
 		screenToViewPort.mapRect(screen);
-		drawFrame(screen);
+		return screen;
 	}
 
 	@Override
@@ -140,34 +153,52 @@ public abstract class GraphView extends View {
 	}
 
 	protected abstract void drawGraph(Canvas canvas, RectF viewPort);
-	
+
 
 	private void drawFrame(final RectF viewport) {
-		if(mDrawFuture != null){
-			mDrawFuture.cancel(false);
+		if(mBackgroundDrawTask != null){
+			mBackgroundDrawTask.mCanceled = true;
 		}
-		final int width = getMeasuredWidth();
-		final int height = getMeasuredHeight();
-		mDrawFuture = BackgroundTask.runBackgroundTask(new BackgroundTask() {
-			private Bitmap mDrawBuffer;
+		mBackgroundDrawTask = new BackgroundDrawTask(getMeasuredWidth(), getMeasuredHeight(), viewport);
+		BackgroundTask.runBackgroundTask(mBackgroundDrawTask, mDrawThread);
+	}
 
-			@Override
-			public void onBackground() {
+	private class BackgroundDrawTask extends BackgroundTask {
+
+		private int width;
+		private int height;
+		private Bitmap mDrawBuffer;
+		private boolean mCanceled = false;
+		private final RectF viewport;
+
+		public BackgroundDrawTask(int width, int height, RectF viewport){
+			this.width = width;
+			this.height = height;
+			this.viewport = new RectF(viewport);
+		}
+
+		@Override
+		public void onBackground() {
+			if(!mCanceled){
 				mDrawBuffer = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
 				drawGraph(new Canvas(mDrawBuffer), viewport);
 			}
+		}
 
-			@Override
-			public void onFinished() {
+		@Override
+		public void onFinished() {
+			if(!mCanceled){
 				mFrontBuffer = mDrawBuffer;
 				mViewPort = viewport;
 				mTransformMatrix.reset();
 				invalidate();
+				//mBackgroundDrawTask = null;
 			}
+		}
 
-		}, mDrawThread);
+
 	}
-	
+
 	private GestureDetector.SimpleOnGestureListener mSimpleGestureListener = new GestureDetector.SimpleOnGestureListener(){
 
 		@Override
@@ -177,13 +208,120 @@ public abstract class GraphView extends View {
 
 		@Override
 		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-			
 			mTransformMatrix.postTranslate(-distanceX, -distanceY);
 			invalidate();
+			updateViewport();
 			return true;
 		}
-		
+
 	};
+
+	private ScaleGestureDetector.SimpleOnScaleGestureListener mSimpleScaleGestureListener = new ScaleGestureDetector.SimpleOnScaleGestureListener(){
+
+		@Override
+		public boolean onScale(ScaleGestureDetector detector) {
+			float scale = detector.getScaleFactor();
+			mTransformMatrix.postScale(scale, scale, detector.getFocusX(), detector.getFocusY());
+			invalidate();
+			updateViewport();
+			return true;
+
+		}
+
+	};
+
+	protected void drawAxis(Canvas canvas, RectF viewPort) {
+
+		DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+		float[] points;
+
+		final int canvasWidth = canvas.getWidth();
+		final int canvasHeight = canvas.getHeight();
+
+		Matrix matrix = getViewportToScreenMatrix(canvasWidth, canvasHeight, viewPort); 
+
+		//clear the canvas
+		canvas.drawColor(mBackgroundColor);
+
+		Paint axisPaint = new Paint();
+		axisPaint.setColor(mAxisColor);
+		axisPaint.setStrokeWidth(2);
+
+		if(mDrawXAxis){
+			//draw X axis
+			points = new float[]{
+					viewPort.left, 0,
+					viewPort.right, 0
+			};
+			matrix.mapPoints(points);
+			canvas.drawLines(points, axisPaint);
+
+			float xPoint = 	(float) (mXAxisDevision *  Math.floor(viewPort.left / mXAxisDevision));
+			while(xPoint < viewPort.right){
+				if(xPoint != 0.0f){
+					points[0] = xPoint;
+					points[1] = 0;
+					points[2] = xPoint;
+					points[3] = 0;
+					matrix.mapPoints(points);
+					points[1] -= TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, metrics);
+					points[3] += TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, metrics);
+					canvas.drawLines(points, axisPaint);
+
+					String label = String.valueOf(xPoint);
+					float textWidth = mAxisLabelPaint.measureText(label);
+					canvas.drawText(label,
+							points[0]-textWidth/2,
+							points[1] - TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, metrics),
+							mAxisLabelPaint);
+
+				}
+				xPoint += mXAxisDevision;
+
+			}
+
+
+		}
+
+		Rect bounds = new Rect();
+		if(mDrawYAxis){
+			//draw Y axis
+			points = new float[]{
+					0, viewPort.top,
+					0, viewPort.bottom
+			};
+			matrix.mapPoints(points);
+			canvas.drawLines(points, axisPaint);
+
+			float yPoint = 	(float) (mYAxisDevision *  Math.floor(viewPort.top / mXAxisDevision));
+			while(yPoint < viewPort.bottom){
+				if(yPoint != 0.0f){
+					points[0] = 0;
+					points[1] = yPoint;
+					points[2] = 0;
+					points[3] = yPoint;
+					matrix.mapPoints(points);
+					points[0] -= TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, metrics);
+					points[2] += TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, metrics);
+					canvas.drawLines(points, axisPaint);
+
+					String label = String.valueOf(yPoint);
+					mAxisLabelPaint.getTextBounds(label, 0, label.length(), bounds);
+					float textWidth = mAxisLabelPaint.measureText(label);
+					canvas.drawText(label,
+							points[0]-textWidth-TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, metrics),
+							points[1]+bounds.height()/2,
+							mAxisLabelPaint);
+				}
+				yPoint += mXAxisDevision;
+			}
+
+		}
+
+
+
+
+	}
 
 
 
--- src/com/devsmart/plotter/LineGraphView.java
+++ src/com/devsmart/plotter/LineGraphView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Matrix.ScaleToFit;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -30,61 +29,11 @@ public class LineGraphView extends GraphView {
 
 	protected void drawGraph(Canvas canvas, RectF viewPort){
 
-		float[] points;
-
-		final int canvasWidth = canvas.getWidth();
-		final int canvasHeight = canvas.getHeight();
-
-		Matrix matrix = getViewportToScreenMatrix(canvasWidth, canvasHeight, viewPort); 
+		drawAxis(canvas, viewPort);
 		
-		//clear the canvas
-		canvas.drawColor(mBackgroundColor);
-
-		Paint axisPaint = new Paint();
-		axisPaint.setColor(mAxisColor);
-		axisPaint.setStrokeWidth(2);
-
-		if(mDrawXAxis){
-			//draw X axis
-			points = new float[]{
-					viewPort.left, 0,
-					viewPort.right, 0
-			};
-			matrix.mapPoints(points);
-			canvas.drawLines(points, axisPaint);
-			
-			points[0] = viewPort.left;
-			points[1] = 0;
-			matrix.mapPoints(points);
-			canvas.drawText(String.valueOf(viewPort.left), points[0], points[1], mAxisLabelPaint);
-			
-			points[0] = viewPort.right;
-			points[1] = 0;
-			matrix.mapPoints(points);
-			canvas.drawText(String.valueOf(viewPort.right), points[0], points[1], mAxisLabelPaint);
-		}
-
-		if(mDrawYAxis){
-			//draw Y axis
-			points = new float[]{
-					0, viewPort.top,
-					0, viewPort.bottom
-			};
-			matrix.mapPoints(points);
-			canvas.drawLines(points, axisPaint);
-			
-			points[0] = 0;
-			points[1] = viewPort.top;
-			matrix.mapPoints(points);
-			canvas.drawText(String.valueOf(viewPort.top), points[0], points[1], mAxisLabelPaint);
-			
-			points[0] = 0;
-			points[1] = viewPort.bottom;
-			matrix.mapPoints(points);
-			canvas.drawText(String.valueOf(viewPort.bottom), points[0], points[1], mAxisLabelPaint);
-		}
+		Matrix matrix = getViewportToScreenMatrix(canvas.getWidth(), canvas.getHeight(), viewPort); 
 		
-		points = new float[2];
+		float[] points = new float[2];
 		for(Series series : mSeries){
 			for(int i=0;i<series.getLength();i++){
 				series.getPoint(i, points);
